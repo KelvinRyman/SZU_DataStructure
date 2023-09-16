a82966d3fe96c6f8142fabb55d85ad1330fdcbb2
Myouren <109898324+KelvinRyman@users.noreply.github.com>
2023-09-16 15:29:09 +0800

    feat: Add solution for C03F

--- Class_03/C03F.cpp
+++ Class_03/C03F.cpp
@@ -1,86 +1,66 @@
-#include <iostream>
-#include <stack>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isValid(const int &x, const int &y, const int &n, const vector<vector<int>> &maze) {
-  return x >= 0 && x < n && y >= 0 && y < n && maze[x][y] == 0;
-}
-
-struct Point {
+struct point {
   int x, y;
 };
 
-stack<Point> path;
-string output_path;
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
 
-int dx[] = {1, -1, 0, 0};
-int dy[] = {0, 0, 1, -1};
+void solve() {
+  int n;
+  cin >> n;
+  vector<vector<int>> maze(n, vector<int>(n));
+  for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) cin >> maze[i][j];
+  vector<vector<int>> vis(n, vector<int>(n, 0));
 
-bool findThePath(const int &n, const vector<vector<int>> &maze) {
-  bool visited[n][n];
-  Point start = {0, 0};
-  Point end = {n - 1, n - 1};
+  stack<point> path, temp;
+  path.push({0, 0});
+  vis[0][0] = 1;
 
-  stack<Point> path1;
-  stack<Point> st;
-  st.push(start);
-  visited[start.x][start.y] = true;
+  while (!path.empty()) {
+    point top = path.top();
+    if (top.x == n - 1 && top.y == n - 1) break;
 
-  while (!st.empty()) {
-    Point curr = st.top();
-    st.pop();
-    path.push(curr);
-    path1.push(curr);
-
-    if (curr.x == end.x && curr.y == end.y) {
-      int count = 0;
-      while (!path1.empty()) {
-        Point p = path1.top();
-        path1.pop();
-        output_path += "[" + to_string(p.x) + ", " + to_string(p.y) + "]" + "--";
-        count++;
-        if (count % 4 == 0) {
-          output_path += '\n';
-        }
-      }
-//      reverse(output_path.begin(), output_path.end());
-      output_path += "END";
-      return true;
-    }
-
-    for (int i = 0; i < 4; i++) {
-      int nx = curr.x + dx[i];
-      int ny = curr.y + dy[i];
-      if (isValid(nx, ny, n, maze) && !visited[nx][ny]) {
-        Point next = {nx, ny};
-        st.push(next);
-        visited[nx][ny] = true;
+    bool flag = false;
+    for (int i = 0; i < 4; ++i) {
+      int nx = top.x + dx[i], ny = top.y + dy[i];
+      if (nx >= 0 && nx < n && ny >= 0 && ny < n && maze[nx][ny] == 0 && !vis[nx][ny]) {
+        path.push({nx, ny});
+        vis[nx][ny] = 1;
+        flag = true;
+        break;
       }
     }
+
+    if (!flag) path.pop();
   }
 
-  return false;
+  while (!path.empty()) {
+    temp.push(path.top());
+    path.pop();
+  }
+
+  int i = 0;
+  while (!temp.empty()) {
+    point cpos = temp.top();
+    temp.pop();
+    if (++i % 4 == 0) cout << '[' << cpos.x << ',' << cpos.y << ']' << "--\n";
+    else cout << '[' << cpos.x << ',' << cpos.y << ']' << "--";
+  }
+
+  if (i == 0) cout << "no path\n";
+  else cout << "END\n";
 }
 
 int main() {
-  int t, n;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
   cin >> t;
-  while (t--) {
-    cin >> n;
-    vector<vector<int>> maze(n, vector<int>(n));
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; ++j) {
-        cin >> maze[i][j];
-      }
-    }
-    bool status = findThePath(n, maze);
-    if (status) {
-      cout << output_path;
-    } else {
-      cout << "no path";
-    }
-  }
+  while (t--) solve();
+
   return 0;
 }
