51166c80760e4b046a9cbed358db9d1592ad0fce
Myouren <109898324+KelvinRyman@users.noreply.github.com>
2023-09-17 17:12:32 +0800

    feat: Add unstable algorithm for C03E

--- Class_03/C03E.cpp
+++ Class_03/C03E.cpp
@@ -1,21 +1,13 @@
-#include <iostream>
-#include <stack>
-#include <string>
-#include <cstring>
-#include <cstdlib>
-#include <iomanip>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-const int OK = 0;
-const int ERROR = -1;
-const int OVERFLOW = -1;
-const int OPSETSIZE = 7;
-typedef int Status;
-
-class EX_Calculate {
+class Expression {
+ public:
+  Expression();
+  double evaluateExpression();
  private:
-  const char Prior[7][7] = {
+  static double calculate(double Operand1, double Operand2, char Operator);
+  const char PRIOR[7][7] = {
       {'>', '>', '<', '<', '<', '>', '>'},
       {'>', '>', '<', '<', '<', '>', '>'},
       {'>', '>', '>', '>', '<', '>', '>'},
@@ -24,13 +16,96 @@ class EX_Calculate {
       {'>', '>', '>', '>', ' ', '>', '>'},
       {'<', '<', '<', '<', '<', ' ', '='}
   };
-  const char Opset[OPSETSIZE] = {'+', '-', '*', '/', '(', ')', '#'};
- public:
-  float Operate(float a, unsigned char theta, float b);
-  Status In(char Test, char *TesOp);
-  char precede(char Aop, char Bop);
+  const char OPERATOR_SET[7] = {'+', '-', '*', '/', '(', ')', '#'};
+  string exp;
+};
+
+Expression::Expression() {
+  getline(cin, exp);
+}
+
+double Expression::calculate(double Operand1, double Operand2, char Operator) {
+  switch (Operator) {
+    case '+' : return Operand1 + Operand2;
+    case '-' : return Operand1 - Operand2;
+    case '*' : return Operand1 * Operand2;
+    case '/' :
+      if (Operand2 != 0) {
+        return Operand1 / Operand2;
+      } else {
+        cerr << "Division by zero.";
+      }
+    default: return 0;
+  }
+}
+
+double Expression::evaluateExpression() {
+  stack<char> operators;
+  stack<double> operands;
+
+  string str = exp;
+  while (!str.empty()) {
+    if ((str[0] >= '0' && str[0] <= '9') || str[0] == '.') {
+      // 当字符为数字或者浮点数的时候，将其压入操作数栈
+      string num = "";
+      while ((str[0] >= '0' && str[0] <= '9') || str[0] == '.') {
+        num += str[0];
+        str.erase(0, 1);
+      }
+
+      char *endPtr;
+      operands.push(strtod(num.c_str(), &endPtr));
+
+      if (*endPtr != '\0') {
+        cerr << "error of strtod\n";
+        return -1;
+      }
+
+    } else if (operators.empty()) {
+      // 当操作符栈为空的时候，将操作符压入操作符栈
+      operators.push(str[0]);
+      str.erase(0, 1);
+
+    } else {
+      int operator_1, operator_2;
+      for (int i = 0; i < 7; ++i) {
+        if (OPERATOR_SET[i] == operators.top()) {
+          operator_1 = i;
+          break;
+        }
+      }
+      for (int i = 0; i < 7; ++i) {
+        if (OPERATOR_SET[i] == str[0]) {
+          operator_2 = i;
+          break;
+        }
+      }
+
+      if (PRIOR[operator_1][operator_2] == '<') {
+        operators.push(str[0]);
+        str.erase(0, 1);
+      } else if (PRIOR[operator_1][operator_2] == '>') {
+        double operand_2 = operands.top();
+        operands.pop();
+        double operand_1 = operands.top();
+        operands.pop();
+        operands.push(calculate(operand_1, operand_2, operators.top()));
+        operators.pop();
+      } else {
+        str.erase(0, 1);
+        operators.pop();
+      }
+    }
+  }
+  return operands.top();
 };
 
 int main() {
-
+  int t;
+  cin >> t;
+  while (t--) {
+    Expression exp;
+    cout << fixed << setprecision(4) << exp.evaluateExpression() << '\n';
+  }
+  return 0;
 }
